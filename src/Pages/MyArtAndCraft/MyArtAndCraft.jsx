fd63ed4b922fff4f9b67050423e54c471cfd2fd8
razzak118348 <abdurrazzak118348@gmail.com>
2024-11-05 21:22:31 +0600

    craftDetails

--- src/Pages/MyArtAndCraft/MyArtAndCraft.jsx
+++ src/Pages/MyArtAndCraft/MyArtAndCraft.jsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import Swal from "sweetalert2";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const MyArtAndCraft = () => {
+
+    const [allCraft, SetAllCraft] = useState([])
+
+    useEffect(() => {
+      fetch('http://localhost:5000/allCraft')
+        .then(res => res.json())
+        .then(data => SetAllCraft(data))
+    }, [allCraft])
+
+    const handleDelete = (id) => {
+      console.log(id);
+
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          fetch(`http://localhost:5000/allCraft/${_id}`, {
+            method: "DELETE",
+          })
+            .then(res => res.json())
+            .then(data => {
+              if (data.deletedCount) {
+
+                const remainingCraft = allCraft.filter(craft => craft._id != id)
+                SetAllCraft(remainingCraft)
+                Swal.fire({
+                  title: "Deleted!",
+                  text: "Your Craft has been deleted.",
+                  icon: "success"
+                });
+              }
+
+            })
+
+        }
+      });
+
+    }
+
     return (
         <div>
-my art and craft
+<Link to={`/update/${_id}`}><button className="btn bg-sky-400 mt-9">Update</button></Link>
+<button onClick={() => handleDelete(_id)} className="btn bg-red-500 mx-10">Delete</button>
         </div>
     );
 };
